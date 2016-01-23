a58757f54efd3c98a5efd314252c13854b479aa7
peter <petrel.harp@gmail.com>
2016-01-22 21:08:42 -0800

    removed unnecessary steps from POPRES_cov.R

--- popres/POPRES_cov.R
+++ popres/POPRES_cov.R
@@ -5,11 +5,10 @@
 #PBS -o /home/cmb-11/plr/hli465/POPRES/output
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=100gb,pmem=100gb,vmem=100gb
-coded=as.matrix(read.table("/home/cmb-11/plr/hli465/POPRES/coded_data_chr1.txt"))
-M=rowMeans(coded,na.rm=TRUE)
-M=rep(M,times=ncol(coded))
-M=matrix(M,nrow=nrow(coded),ncol=ncol(coded),byrow=FALSE)
-coded=coded-M
-cov=cov(coded,use="pairwise")
+
+coded <- as.matrix(read.table("/home/cmb-11/plr/hli465/POPRES/coded_data_chr1.txt"))
+M <- rowMeans(coded,na.rm=TRUE)
+coded <- coded-M
+cov <- cov(coded,use="pairwise")
 write.table(cov,"/home/cmb-11/plr/hli465/POPRES/cov_data_for_chr1.txt",sep="\t")
 
