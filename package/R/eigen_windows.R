bcdcacaa333024d4f11afd43734e92973767849f
peter <petrel.harp@gmail.com>
2016-05-05 05:45:15 -0700

    rewrote eigen_windows to take functions

--- package/R/eigen_windows.R
+++ package/R/eigen_windows.R
@@ -6,11 +6,10 @@
 #' and returns the given number of eigenvector/eigenvalue pairs.
 #' Omits the last (short) window.
 #'
-#' @param data The object containing the genotype data, e.g., a numeric matrix.
-#' @param win.fn Function that takes \code{data} and a positive integer \code{n} and returns the numeric matrix that correspond to the \code{n}th window.
-#' @param do.windows Vector of integers (or, arguments to \code{win.fn}) that correspond to the windows used.
-#' @param win If \code{win.fn} is not provided, each contiguous block of \code{win} rows of the matrix \code{data} will be one window.
+#' @param data Either a numeric matrix or a window extractor function, that takes a positive integer \code{n} and returns the numeric matrix that correspond to the \code{n}th window.
 #' @param k Number of eigenvalue/eigenvector pairs to return.
+#' @param do.windows Vector of integers that correspond to the windows used.
+#' @param win If \code{data} is a matrix, each contiguous block of \code{win} rows of the matrix \code{data} will be one window.
 #' @param mc.cores If this is greater than 1, parallel::mclapply will be used.
 #' @param ... Other parameters to be passed to \code{win.fn}.
 #' @return A named list, with components
@@ -18,14 +17,41 @@
 #'   $vectors : A numeric matrix, one column for each windows, having the eigenvectors in order (so it has k * ncol(matrix) rows).
 #' @export
 eigen_windows <- function ( 
-            data, 
-            win.fn=function(data,n,...){ data[seq( ((n-1)*win+1), (n*win) ),] }, 
-            do.windows=1:floor(nrow(data)/win),
-            win, k, mc.cores=1, ... ) {
-    if (missing(win) && missing(win.fn)) { stop("Must supply either win or win.fn.") }
+            data, k,
+            do.windows=NULL,
+            win, mc.cores=1, ... ) {
     this.lapply <- if (mc.cores>1) { function (...) parallel::mclapply(...,mc.cores=mc.cores) } else { lapply }
+    eigen.mat <- if (inherits(data,"function")) {
+                eigen_windows_winfn( win.fn=data, do.windows=do.windows, k=k, mc.cores=mc.cores, ... )
+            } else {
+                if (missing(win)) { stop("Must supply either a function or a matrix and a value for win.") }
+                eigen_windows_matrix( data=data, win=win, do.windows=do.windows, k=k, mc.cores=mc.cores )
+            }
+    return( list(
+            values = eigen.mat[1:k,],
+            vectors = eigen.mat[-(1:k),]
+        ) )
+}
+
+#' Sets up eigen_windows_winfn to work on a matrix.
+eigen_windows_matrix <- function (
+            data, k, win
+            do.windows=NULL,
+            mc.cores ) {
+    win.fn <- function(n,...){ data[seq( ((n-1)*win+1), (n*win) ),] }
+    attr(win.fn,"max.n") <- floor( nrow(data)/win )
+    return( eigen_windows_winfn( win.fn, do.windows=do.windows, k=k, mc.cores=mc.cores ) )
+}
+
+#' Does the work of eigen_windows.
+eigen_windows_winfn <- function ( 
+            win.fn=function(data,n,...){ data[seq( ((n-1)*win+1), (n*win) ),] }, 
+            do.windows=NULL,
+            k, mc.cores=1, ... ) {
+    this.lapply <- if (mc.cores>1) { function (...) parallel::mclapply(...,mc.cores=mc.cores) } else { lapply }
+    if (is.null(do.windows)) { do.windows <- seq_len(attr(win.fn,"max.n")) }
     .local <- function(x) {
-        chunk <- win.fn(data,n=x,...)
+        chunk <- win.fn(n=x,...)
         chunk <- sweep( chunk, 1, rowMeans(chunk,na.rm=TRUE) )
         cov <- cov(chunk,use="pairwise")
         if(any(is.na(cov))) {return(rep(NA,2*(nrow(cov)+1)))}
@@ -34,8 +60,5 @@ eigen_windows <- function (
         return( c( PCA$values[1:k], PCA$vectors[,1:k] ) )
     }
     eigen.mat <- do.call( cbind, this.lapply( do.windows, .local ) )
-    return( list(
-            values = eigen.mat[1:k,],
-            vectors = eigen.mat[-(1:k),]
-        ) )
+    return( eigen.mat )
 }
