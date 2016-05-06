35f3a968b0711f6b338fae4fb487ebe68113d210
peter <petrel.harp@gmail.com>
2016-05-06 14:32:37 -0700

    added manpages

--- medicago/run_on_medicago.R
+++ medicago/run_on_medicago.R
@@ -4,7 +4,17 @@ library(optparse)
 invocation <- commandArgs()
 
 usage <- "\
-Does a local PCA analysis on the Medicago truncatula hapmap data (see medicago_data_setup.html for details).
+\
+Does a local PCA analysis on the Medicago truncatula hapmap data (see medicago_data_setup.html for details). \
+To run an analysis on windows of 1000 SNPs, for instance: \
+    Rscript run_on_medicago.R -t snp -s 1000 \
+while to run it on windows of 1000 bps: \
+    Rscript run_on_medicago.R -t bp -s 1000 \
+\
+Do
+    Rscript run_on_medicago.R -h\
+for options.\
+\
 "
 
 option_list <- list(
@@ -13,11 +23,13 @@ option_list <- list(
         make_option( c("-s","--size"),   type="integer",               help="Size of the window, in units of type."),
         make_option( c("-k","--npca"),   type="integer",   default=2L, help="Number of principal components to compute for each window. [default: %default]"),
         make_option( c("-m","--nmds"),   type="integer",   default=2L, help="Number of principal coordinates (MDS variables) to compute. [default: %default]"),
-        make_option( c("-o","--outdir"), type="character",             help="Directory to save results to.  [default: lostruct/results_%type_%size_%jobid/]"),
+        make_option( c("-o","--outdir"), type="character",             help="Directory to save results to.  [default: lostruct/results_type_%type_size_%size_jobid_%jobid/]"),
         make_option( c("-j","--jobid"),  type="character", default=formatC(1e6*runif(1),width=6,format="d",flag="0"),   help="Unique job id. [default random]")
     )
 opt <- parse_args(OptionParser(option_list=option_list,description=usage))
-if (is.null(opt$outdir)) { opt$outdir <- file.path("lostruct", sprintf( "results_%s_%d_%s", opt$type, opt$size, opt$jobid ) ) }
+if (is.null(opt$outdir)) { opt$outdir <- file.path("lostruct", sprintf( "results_type_%s_size_%d_jobid_%s", opt$type, opt$size, opt$jobid ) ) }
+if (is.null(opt$type) || is.null(opt$size)) { stop(usage) }
+
 print(opt) # this will go in the logfile
 Sys.time()
 
@@ -35,7 +47,7 @@ all.pcas <- numeric(0)
 
 # local PCA, by chromosome
 for (bcf.file in bcf.files) {
-    pca.file <- file.path( opt$outdir, sprintf(gsub(".bcf",".pca.csv",bcf.file)) )
+    pca.file <- file.path( opt$outdir, sprintf(gsub(".bcf",".pca.csv",basename(bcf.file))) )
     if (file.exists(pca.file)) { stop(paste("File",pca.file,"already exists! Not overwriting.")) }
     cat("Finding PCs for", bcf.file, "and writing out to", pca.file, "\n")
     win.fn <- vcf_windower(bcf.file, size=opt$size, type=tolower(opt$type) )
