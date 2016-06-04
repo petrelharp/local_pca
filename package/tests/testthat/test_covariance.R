fedbbcf15be2346599f98fef9d4c94df3f4a40ed
peter <petrel.harp@gmail.com>
2016-06-03 19:48:11 -0700

    various edits

--- package/tests/testthat/test_covariance.R
+++ package/tests/testthat/test_covariance.R
@@ -21,11 +21,20 @@ cov_fn <- function (x,y=x) {
     sum( (u-mean(u))*(v-mean(v)) )/(sum(ut)-1)
 }
 
-my.cov <- matrix(0,nrow=nrow(cov.x),ncol=ncol(cov.x))
+cov_fn_2 <- function (x,y=x) { 
+    ut <- (!is.na(x)) & (!is.na(y))
+    u <- x[ut]; 
+    v <- y[ut]; 
+    sum( u*v )/(sum(ut)-1) - sum(u)*sum(v)/(sum(ut)*(sum(ut)-1))
+}
+
+my.cov.2 <- my.cov <- matrix(0,nrow=nrow(cov.x),ncol=ncol(cov.x))
 for (i in 1:ncol(x)) {
     for (j in 1:ncol(x)) {
         my.cov[i,j] <- cov_fn( x[,i], x[,j] )
+        my.cov.2[i,j] <- cov_fn_2( x[,i], x[,j] )
     }
 }
 
 expect_equal( my.cov, cov.x )
+expect_equal( my.cov.2, cov.x )
