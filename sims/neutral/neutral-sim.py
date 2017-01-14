e3ab5d5ae1375f0930a2a9b4ca4784e6feb5cea3
peter <petrel.harp@gmail.com>
2017-01-14 00:02:32 -0800

    neutral simulation script

---
+++ sims/neutral/neutral-sim.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3.5
+description = '''
+Simulate.
+'''
+
+import gzip
+import sys
+from optparse import OptionParser
+import math
+import time
+
+import msprime
+
+def fileopt(fname,opts):
+    '''Return the file referred to by fname, open with options opts;
+    if fname is "-" return stdin/stdout; if fname ends with .gz run it through gzip.
+    '''
+    if fname == "-":
+        if opts == "r":
+            fobj = sys.stdin
+        elif opts == "w":
+            fobj = sys.stdout
+        else:
+            print("Something not right here.")
+    elif fname[len(fname)-3:len(fname)]==".gz":
+        fobj = gzip.open(fname,opts)
+    else:
+        fobj = open(fname,opts)
+    return fobj
+
+parser = OptionParser(description=description)
+parser.add_option("-k","--nsamples",dest="nsamples",help="number of samples, total")
+parser.add_option("-N","--popsize",dest="popsize",help="size of each subpopulation",default=100)
+parser.add_option("-w","--width",dest="width",help="width of square grid, in populations",default=3)
+parser.add_option("-L","--length",dest="length",help="number of bp in the chromosome",default=1e4)
+parser.add_option("-m","--migr",dest="migr",help="migration proportion between adjacent populations",default=.01)
+parser.add_option("-r","--min_recomb",dest="min_recomb",help="minimum recombination rate",default=2.5e-8)
+parser.add_option("-R","--max_recomb",dest="max_recomb",help="maximum recombination rate",default=2.5e-8)
+parser.add_option("-p","--mapfile",dest="mapfile",help="name of map file")
+parser.add_option("-u","--mut_rate",dest="mut_rate",help="mutation rate",default=1e-7)
+parser.add_option("-o","--outfile",dest="outfile",help="name of output file (or '-' for stdout)",default="-")
+parser.add_option("-g","--logfile",dest="logfile",help="name of log file (or '-' for stdout)",default="-")
+(options,args) =  parser.parse_args()
+
+outfile = fileopt(options.outfile, "w")
+logfile = fileopt(options.logfile, "w")
+
+logfile.write("Options:\n")
+logfile.write(str(options)+"\n")
+
+popsize = float(options.popsize)
+width = int(options.width)
+nsamples = int(options.nsamples)
+length = float(options.length)
+migr = float(options.migr)
+mut_rate = float(options.mut_rate)
+if options.mapfile is not None:
+    use_map = True
+    mapfile = fileopt(options.mapfile, "r")
+    positions=[]
+    rates=[]
+    mapfile.readline()
+    for line in mapfile:
+        pos, rate, = map(float, line.split()[1:3])
+        positions.append(pos)
+        # Rate is expressed in centimorgans per megabase, which
+        # we convert to per-base rates
+        rates.append(rate * 1e-8)
+    recomb_map = msprime.RecombinationMap(positions,rates)
+else:
+    min_recomb = float(options.min_recomb)
+    max_recomb = float(options.max_recomb)
+    use_map = (min_recomb==max_recomb)
+    # recombination map
+    n_recomb_steps = 100
+    recomb_map = msprime.RecombinationMap(
+            positions=[ k*length/n_recomb_steps for k in range(n_recomb_steps+1) ],
+            rates=[ max_recomb - (max_recomb-min_recomb)*k/n_recomb_steps for k in range(n_recomb_steps+1) ])
+
+logfile.write(time.strftime('%X %x %Z')+"\n")
+logfile.write("Beginning simulation:\n")
+
+# population setup
+per_samples = math.ceil(nsamples/width/width)
+pop_config = [ msprime.PopulationConfiguration(sample_size=per_samples) for i in range(width) for j in range(width) ]
+mig_mat = [ [ migr if abs(i-k)+abs(j-l)==1 else 0.0 for i in range(width) for j in range(width) ] for k in range(width) for l in range(width) ]
+
+logfile.write("Sample configuration:\n")
+logfile.write(str([x.sample_size for x in pop_config])+"\n")
+# logfile.write("Migration matrix:\n")
+# logfile.write(str(mig_mat)+"\n")
+
+if width==1:
+    if not use_map:
+        tree_sequence = msprime.simulate(
+                sample_size=nsamples, 
+                Ne=popsize,
+                length=length, 
+                recombination_rate=max_recomb,
+                mutation_rate=mut_rate)
+    else:
+        tree_sequence = msprime.simulate(
+                sample_size=nsamples, 
+                Ne=popsize,
+                recombination_map=recomb_map,
+                mutation_rate=mut_rate)
+
+else:
+    if not use_map:
+        tree_sequence = msprime.simulate(
+                population_configurations=pop_config,
+                migration_matrix=mig_mat,
+                Ne=popsize,
+                length=length,
+                recombination_rate=max_recomb,
+                mutation_rate=mut_rate)
+    else:
+        tree_sequence = msprime.simulate(
+                population_configurations=pop_config,
+                migration_matrix=mig_mat,
+                Ne=popsize,
+                recombination_map=recomb_map,
+                mutation_rate=mut_rate)
+
+logfile.write("Done!\n")
+logfile.write(time.strftime('%X %x %Z')+"\n")
+logfile.write("Mean pairwise diversity: {}\n".format(tree_sequence.get_pairwise_diversity()/length))
+logfile.write("Number of trees: {}\n".format(tree_sequence.get_num_trees()))
+logfile.write("Number of mutations: {}\n".format(tree_sequence.get_num_mutations()))
+
+tree_sequence.write_vcf(outfile,ploidy=1)
+
---
+++ sims/notes.md
@@ -0,0 +1,53 @@
+# Neutral simulations:
+
+In `neutral/`; all simulations on an 10x10 grid, with 2Ne=1000 per population,
+simulating a chromosome of length 4e7bp, mean recombination rate 2.5e-8, mutation rate 1e-7. 
+Total population is 1e5, so we expect divergence of 0.01, and a tree every 400bp.
+To have lineages mix over this time, but not instantly,
+migration rate 4e-3 (in proportion of a population replaced by a given neighbor each generation).
+
+In all cases we sample 1000 individuals.
+
+1. Flat recombination:
+```
+./neutral-sim.py -k 36 -N 10 -w 3 -L 4e7 -m 4e-3 -r 2.5e-8 -R 2.5e-8 -u 1e-7 -o flat_recomb_short.vcf -g flat_recomb_short.log
+./neutral-sim.py -k 1000 -N 1000 -w 10 -L 4e7 -m 4e-3 -r 2.5e-8 -R 2.5e-8 -u 1e-7 -o flat_recomb.vcf -g flat_recomb.log
+```
+
+2. Linearly increasing recombination rate from 0.5e-8 to 4.5e-8 (same total length):
+```
+./neutral-sim.py -k 36 -N 10 -w 3 -L 4e7 -m 4e-3 -r 0.5e-8 -R 4.5e-8 -u 1e-7 -o linear_recomb_short.vcf -g linear_recomb_short.log
+./neutral-sim.py -k 1000 -N 1000 -w 10 -L 4e7 -m 4e-3 -r 0.5e-8 -R 4.5e-8 -u 1e-7 -o linear_recomb.vcf -g linear_recomb.log
+```
+
+3. Hot spottish recombination map (same total length):
+The map we use is deCode's female map for chr7, rescaled to our length:
+```r
+gmap <- read.table('female.gmap',header=TRUE)
+gmap <- subset(gmap, chr=="chr7" & !is.na(cM))
+gmap <- subset(gmap, gmap$cM!=0 & c(gmap$cM[-1]==0,FALSE))
+bp_len <- 4e7
+gmap$pos <- gmap$pos-gmap$pos[1]
+gmap$pos <- floor( gmap$pos*bp_len/max(gmap$pos) )
+gmap$cM <- gmap$cM * 100 / sum(gmap$cM)
+gmap <- subset(gmap,c(TRUE,diff(gmap$pos)>0))
+map <- data.frame(
+    Chromosome=1,
+    "Position(bp)"=gmap$pos,
+    "Rate(cM/Mb)"=c( gmap$cM[-1]*1e6/diff(gmap$pos), 0.0),
+    "Map(cM)"=0) # unused
+write.table(map, file="decode_female_chr7.gmap", row.names=FALSE)
+
+# Chromosome  Position(bp)    Rate(cM/Mb)     Map(cM)
+# chr1        55550   2.981822        0.000000
+# chr1        82571   2.082414        0.080572
+```
+```
+./neutral-sim.py -k 36 -N 10 -w 3 -L 4e7 -m 4e-3 -p decode_female_chr7.gmap -u 1e-7 -o hot_recomb_short.vcf -g hot_recomb_short.log
+./neutral-sim.py -k 1000 -N 1000 -w 10 -L 4e7 -m 4e-3 -p decode_female_chr7.gmap -u 1e-7 -o hot_recomb.vcf -g hot_recomb.log
+```
+
+Note: the deCode map is obtained from http://www.decode.com/addendum/ ; should cite:
+    Kong, A et al.  Fine scale recombination rate differences between sexes, populations and individuals. Nature  467 , 1099–1103 (28 October 2010) doi:10.1038/nature09525.
+This file lists the position and the cM in the *previous* window, 
+while msprime wants the cM/bp over the *next* window.
