13edcd17e3b72ef8ff649540d5d84f04557424e8
Peter Ralph <petrel.harp@gmail.com>
2017-10-11 03:50:11 -0700

    cov tests

--- lostruct/tests/testthat/test_running_cov.R
+++ lostruct/tests/testthat/test_running_cov.R
@@ -1,20 +1,31 @@
 context("running covariance")
 
-xx <- matrix( 1:27, ncol=3 )
+set.seed(123)
+nchunks <- 100
+chunklen <- 5
+xx <- matrix( rpois(nchunks*chunklen*3, 5), ncol=3 )
 
 f <- function (k) {
-    xx[ (k-1)*3+(1:3), ]
+    xx[ (k-1)*chunklen+(1:chunklen), ]
 }
 
-n <- 1:3
+n <- 1:nchunks
 
 expect_equal( xx, do.call(rbind, lapply(n,f) ) )
 
-expect_equal( cov(xx,use="pairwise"), running_cov(f,n) )
+expect_equal( cov(xx,use="pairwise"), running_cov(f,n, normalize.rows=FALSE) )
+
+## with normalize.rows
+
+expect_equal( cov(sweep(xx,1,rowMeans(xx)),use="pairwise"), running_cov(f,n, normalize.rows=TRUE) )
+
+Imat <- diag(3) - 1/3
+expect_equal( Imat%*%cov(xx,use="pairwise")%*%Imat, running_cov(f,n, normalize.rows=TRUE) )
 
 ## with missings
 
-xx[1,2] <- xx[2,2] <- xx[1,3] <- xx[6,1] <- xx[5,2] <- NA
+xx[1,2] <- xx[2,2] <- xx[1,3] <- xx[6,1] <- xx[7,2] <- NA
 
-expect_equal( cov(xx,use="pairwise"), running_cov(f,n) )
+expect_equal( xx, do.call(rbind, lapply(n,f) ) )
 
+expect_equal( cov(xx,use="pairwise"), running_cov(f,n, normalize.rows=FALSE) )
