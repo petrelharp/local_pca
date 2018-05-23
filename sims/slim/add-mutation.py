6e7878398a467ec82b13571e831ca4eafe414dc6
peter <petrel.harp@gmail.com>
2018-05-23 13:49:57 -0700

    get indivs

--- sims/slim/add-mutation.py
+++ sims/slim/add-mutation.py
@@ -73,8 +73,8 @@ def write_vcf(chrom):
     seed = seeds[chrom]
     logfile.write("Simulating mutations on" + treefile + "\n")
     ts = msprime.load(treefile)
-    rng = msprime.RandomGenerator(seed)
     tables = ts.dump_tables()
+    rng = msprime.RandomGenerator(seed)
     mutgen = msprime.MutationGenerator(rng, mut_rate)
     mutgen.generate(tables.nodes, tables.edges, tables.sites, tables.mutations)
     logfile.write("Saving to" + args.vcffile[chrom] + "\n")
---
+++ sims/slim/get-individuals.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+description = '''
+Get, and dump to text, the individuals table in the .trees output from a SLiM simulation.
+'''
+
+import sys, os
+import gzip
+import glob
+import re
+import argparse
+import struct
+
+import msprime
+
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("--tree_file", "-t", type=str, nargs="*", dest="tree_file", 
+                    help="name of file to load tree sequences from [default: .trees files in basedir.")
+parser.add_argument("--basedir", "-o", type=str, dest="basedir", 
+                    help="name of directory to save output files to.")
+parser.add_argument("--indivfile", "-i", type=str, nargs="*", dest="indiv", 
+                    help="name of output files [default: as trees but with .indiv.tsv]")
+parser.add_argument("--logfile", "-g", type=str, dest="logfile", 
+                    help="name of log file")
+
+args = parser.parse_args()
+argdict = vars(args)
+
+if args.basedir is None and args.indivfile is None:
+    print(description)
+    raise ValueError("Must specify at least basedir and indivfile (run with -h for help).")
+
+if args.indivfile is None or len(args.indivfile) == 0:
+    args.indivfile = [os.path.join(args.basedir, re.sub("[.]trees$", "", os.path.basename(x))) 
+                        + ".indiv.tsv" for x in args.tree_file]
+
+if args.logfile is None:
+    args.logfile = os.path.join(args.basedir, "add_muts.log")
+
+assert len(args.indivfile) == len(args.tree_file)
+
+logfile = open(args.logfile, "w")
+
+# typedef struct __attribute__((__packed__)) {
+# 	slim_pedigreeid_t pedigree_id_;	// 8 bytes (int64_t): the SLiM pedigree ID for this individual, assigned by pedigree rec
+# 	slim_age_t age_; // 4 bytes (int32_t)
+# 	slim_objectid_t subpopulation_id_; // 4 bytes (int32_t)
+# } IndividualMetadataRec;
+
+class slimIndividual(object):
+    def __init__(self, table_row):
+        ped_id, age, subpop = struct.unpack("qii", table_row.metadata)
+        location = table_row.location[:3]
+        self.ped_id = ped_id
+        self.age = age
+        self.subpop = subpop
+        self.location = location
+
+
+node_inds = [np.where(ts.tables.nodes.individual == u)[0] 
+             for u in range(ts.tables.individuals.num_rows)]
+
+header = ["id", "age", "subpop", "location_x", "location_y", "location_z", "node_ma", "node_pa"]
+
+def write_indivs(chrom):
+    treefile = args.tree_file[chrom]
+    out = open(args.indivfile[chrom], "w")
+    logfile.write("Reading trees from " + treefile + "\n")
+    ts = msprime.load(treefile)
+    tables = ts.dump_tables()
+    individuals = [slimIndividual(ts.tables.individuals[k]) 
+                   for k in range(ts.tables.individuals.num_rows)]
+    logfile.write("Saving to" + args.indivfile[chrom] + "\n")
+    out.write("\t".join(header) + "\n");
+    for k, ind in enumerate(individuals):
+        data = [ind.ped_id, ind.age, ind.subpop] + list(ind.location) + list(node_inds[k])
+        out.write("\t".join(map(str, data)) + "\n")
+    out.close()
+    return True
+
+for k in range(len(args.tree_file)):
+    write_indivs(k)
+
+logfile.write("Done!\n")
+logfile.close()
+
